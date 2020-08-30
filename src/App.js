48c2ee9fb848094738481ad6495c1b80cd948b29
lovelaught <darya.bratova13@gmail.com>
2020-08-30 21:13:32 +0300

    remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { FavCoubs } from './containers/FavCoubs';
+import React from "react";
+import { FavCoubs } from "./containers/FavCoubs";
 
-function App() {
-  return (
-    <div>
-    <FavCoubs/>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <FavCoubs />;
+};
