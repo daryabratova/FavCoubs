7d9c6f0e928640a7d3ae125b18959cb6a73b0474
lovelaught <darya.bratova13@gmail.com>
2020-09-02 20:08:49 +0300

    add new coubs

--- src/containers/FavCoubs/data.js
+++ src/containers/FavCoubs/data.js
@@ -1,1 +1,14 @@
-export const videosSrc = ["28xy7m", "28ygku", "29ipbe", "29lc06", "5irf4"];
+export const videosSrc = [
+  "28xy7m",
+  "28ygku",
+  "29ipbe",
+  "29lc06",
+  "5irf4",
+  "2ichu6",
+  "2aej9a",
+  "2fxpgq",
+  "8ckuw",
+  "2d4hcq",
+  "2a6d79",
+  "27onxc",
+];
