211cadc1b3bd3f6ac77ca95d6c89af1aba208535
lovelaught <darya.bratova13@gmail.com>
2020-09-02 19:17:44 +0300

    Updates

---
+++ precache-manifest.0a51de2d56e830fee58ba17b7341c310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e1408befcafb4e9c4c126b98303815",
+    "url": "/FavCoubs/index.html"
+  },
+  {
+    "revision": "c30f9676cfd3d4ef1387",
+    "url": "/FavCoubs/static/css/main.415c6742.chunk.css"
+  },
+  {
+    "revision": "4969c70d801afb5629db",
+    "url": "/FavCoubs/static/js/2.e3bc5be2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FavCoubs/static/js/2.e3bc5be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30f9676cfd3d4ef1387",
+    "url": "/FavCoubs/static/js/main.a3ebc3ad.chunk.js"
+  },
+  {
+    "revision": "c8c384d68cfb7f15c730",
+    "url": "/FavCoubs/static/js/runtime-main.fa13989a.js"
+  }
+]);
